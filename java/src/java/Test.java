baa5b5824012c28352c9a2e6d2aeaacc24ede963
Shreya5777 <shreyanarayankar20@gmail.com>
2025-01-22 14:43:21 +0530

    add to addition by shreya

--- java/src/java/Test.java
+++ java/src/java/Test.java
@@ -1,11 +1,9 @@
 package java;
 
 public class Test {
-	public static void main(String [] args)
+	public void addition()
 	{
-		public void addition()
-			{
-			System.out.println("this is addition");
+		System.out.println("this is addition");
 			
 	
 	}
